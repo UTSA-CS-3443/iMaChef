ca1348c5fe454f67f5f2597993b41a89169d1c8b
tjherron <Thomas.Herron@my.utsa.edu>
2021-11-02 11:09:01 -0500

    initial framework

--- src/application/controller/CookController.java
+++ src/application/controller/CookController.java
@@ -1,5 +1,24 @@
 package application.controller;
 
-public class CookController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+
+public class CookController implements EventHandler<ActionEvent>, Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -1,5 +1,24 @@
 package application.controller;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+
+public class MainController implements EventHandler<ActionEvent>, Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
