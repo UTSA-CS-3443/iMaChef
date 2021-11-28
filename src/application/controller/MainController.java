6946063c3429a2cf54c8793d6d3e5f7a9894ea7e
ProgramRSimms <90295414+ProgramRSimms@users.noreply.github.com>
2021-11-28 17:14:28 -0600

    Update MainController.java
    Just added a little animation to the Main.fxml image that I added

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -1,22 +1,41 @@
 package application.controller;
 
 import java.net.URL;
+import javafx.util.Duration;
 import java.util.ResourceBundle;
-
 import application.Main;
 import application.model.CookBook;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+
+
 
 public class MainController implements EventHandler<ActionEvent>, Initializable{
 
-		
+	@FXML
+	private ImageView mainImage;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		//rotate the main.fxml image
+		RotateTransition rotate = new RotateTransition();
+	 	rotate.setNode(mainImage);
+	 	rotate.setDuration(Duration.millis(1000));
+	 	rotate.setCycleCount(1);
+	 	rotate.setInterpolator(Interpolator.LINEAR);
+	 	rotate.setByAngle(360);
+	 	rotate.setAxis(Rotate.Z_AXIS);
+	 	rotate.play();
+	
 		// TODO Auto-generated method stub
 		if (Main.cookBook == null) {
 			Main.cookBook = CookBook.loadCookBook(Main.DATA_FILE);
@@ -24,7 +43,6 @@ public class MainController implements EventHandler<ActionEvent>, Initializable{
 		Main.recipeSelected = -1; // safety value to make sure a recipe was selected
 		
 	}
-
 	
 	/**
 	 * @param event clicking the "Let's Cook!" button
